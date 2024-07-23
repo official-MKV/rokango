07a1bf2628c90704d18edb7bec284ffc2786323b
official-MKV <vemmaks84@gmail.com>
2024-07-23 15:52:17 +0100

    Done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -19,7 +19,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
+} from "@/Components/ui/alert-dialog";
 
 export function NavBar() {
   const router = useRouter();
